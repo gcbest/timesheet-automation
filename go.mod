7d457914962a819344ad83fe6be034534bdfd7f8
Godfrey Best <godfrey.c.best@gmail.com>
2022-09-13 00:12:58 -0400

    initial commit | accepting user input

---
+++ go.mod
@@ -0,0 +1,8 @@
+module example/hello
+
+go 1.19
+
+require (
+	golang.org/x/sys v0.0.0-20220909162455-aba9fc2a8ff2 // indirect
+	golang.org/x/term v0.0.0-20220722155259-a9ba230a4035 // indirect
+)
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"syscall"
+
+	"golang.org/x/term"
+)
+
+type user struct {
+	username string
+	password string
+}
+
+func getInput(prompt string, isPassword bool, r *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+	if isPassword {
+		bytePassword, err := term.ReadPassword(int(syscall.Stdin))
+		if err != nil {
+			return "", err
+		}
+
+		password := string(bytePassword)
+		return password, nil
+	} else {
+		input, err := r.ReadString('\n')
+		return strings.TrimSpace(input), err
+	}
+
+}
+
+func promptCredentials() (*user, error) {
+	reader := bufio.NewReader(os.Stdin)
+
+	// TODO: add tests here
+	username, error := getInput("Enter username: ", false, reader)
+	if error != nil {
+		fmt.Println("Please enter valid username")
+		promptCredentials()
+	}
+	password, error := getInput("Enter password: ", true, reader)
+
+	// TODO: only prompt for password again
+	if error != nil {
+		fmt.Println("Please enter valid password")
+		promptCredentials()
+	}
+
+	newUser := createUser(username, password)
+	fmt.Println("user", newUser)
+	return newUser, nil
+}
+
+func createUser(username string, password string) *user {
+	newUser := user{
+		username: username,
+		password: password,
+	}
+
+	return &newUser
+}
+
+func main() {
+	promptCredentials()
+}
